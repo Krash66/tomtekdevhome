d3a2b884f59fdb803598dd6a6140d9a3a630ff3c
Krash66 <tomkarasch@tomtekdev.com>
2017-05-21 14:35:33 -0400

    New Email form

---
+++ tomtekdevhome/Models/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace tomtekdevhome.Models
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "You must provide your name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must provide your email."), EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="You must include a subject.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "You must include a message.")]
+        public string Message { get; set; }
+
+    }
+}
