8c0bec2d2a2e24ab54bbbc34786b35eef7918632
Krash66 <tomkarasch@tomtekdev.com>
2017-05-23 21:53:33 -0400

    Background and carosel

--- tomtekdevhome/Controllers/HomeController.cs
+++ tomtekdevhome/Controllers/HomeController.cs
@@ -22,55 +22,55 @@ namespace tomtekdevhome.Controllers
             return View();
         }
 
-        [HttpGet]
+        //[HttpGet]
         public ActionResult Contact()
         {
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Contact(ContactForm vm)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    MailMessage msz = eMailMsg();
-                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
-                                                         //from contact us page 
-                    msz.To.Add("tomtekdev@gmail.com");//Where mail will be sent 
-                    msz.Subject = vm.Subject;
-                    msz.Body = vm.Message;
-                    SmtpClient smtp = new SmtpClient();
+        //[HttpPost]
+        //public ActionResult Contact(ContactForm vm)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            MailMessage msz = eMailMsg();
+        //            msz.From = new MailAddress(vm.Email);//Email which you are getting 
+        //                                                 //from contact us page 
+        //            msz.To.Add("tomtekdev@gmail.com");//Where mail will be sent 
+        //            msz.Subject = vm.Subject;
+        //            msz.Body = vm.Message;
+        //            SmtpClient smtp = new SmtpClient();
 
-                    smtp.Host = "smtp.gmail.com";
+        //            smtp.Host = "smtp.gmail.com";
 
-                    smtp.Port = 587;
+        //            smtp.Port = 587;
 
-                    smtp.Credentials = new System.Net.NetworkCredential
-                    ("tomtekdev@gmail.com", "Bartisgr8");
+        //            smtp.Credentials = new System.Net.NetworkCredential
+        //            ("tomtekdev@gmail.com", "Bartisgr8");
 
-                    smtp.EnableSsl = true;
+        //            smtp.EnableSsl = true;
 
-                    smtp.Send(msz);
+        //            smtp.Send(msz);
 
-                    ModelState.Clear();
-                    ViewBag.Message = "Thank you for Contacting us ";
-                }
-                catch (Exception ex)
-                {
-                    ModelState.Clear();
-                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
-                }
-            }
+        //            ModelState.Clear();
+        //            ViewBag.Message = "Thank you for Contacting us ";
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            ModelState.Clear();
+        //            ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+        //        }
+        //    }
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        private static MailMessage eMailMsg()
-        {
-            return new MailMessage();
-        }
+        //private static MailMessage eMailMsg()
+        //{
+        //    return new MailMessage();
+        //}
 
         public ActionResult Error
         {
